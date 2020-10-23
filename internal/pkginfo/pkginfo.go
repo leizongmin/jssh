a0d5a87c90110f42345bdab415e374f8118ed8cc
Zongmin Lei <leizongmin@gmail.com>
2020-10-23 10:53:03 +0800

    publish: 0.1-commit-443ca79-build-202010231050

--- internal/pkginfo/pkginfo.go
+++ internal/pkginfo/pkginfo.go
@@ -2,4 +2,4 @@ package pkginfo
 
 const Name = "jssh"
 const Version = "0.1"
-const LongVersion = Version + "-commit-7911ceb-build-202010222328"
+const LongVersion = Version + "-commit-443ca79-build-202010231050"
