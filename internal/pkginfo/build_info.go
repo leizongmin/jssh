c7f5ce7a62c16836e2542d82a2c843239d9dbeaa
Zongmin Lei <leizongmin@gmail.com>
2020-10-27 19:01:29 +0800

    chore: build_info 改为模板文件

--- internal/pkginfo/build_info.go
+++ internal/pkginfo/build_info.go
@@ -1,6 +1,6 @@
 package pkginfo
 
-const BuildDate = "20201027"
-const BuildTime = "1855"
-const BuildCommit = "2b39d7b"
+const BuildDate = "YYYYMMDD"
+const BuildTime = "HHmm"
+const BuildCommit = "AAAAAAA"
 const BuildGoVersion = "1.15.3"
