90a7a755d226198bb7f3100db92509b799687ce8
Zongmin Lei <leizongmin@gmail.com>
2020-10-30 12:53:42 +0800

    refactor: REPL api list 无需全局变量和函数名，仅模块内的方法名称需要注册

--- internal/jsshcmd/repl_help.go
+++ internal/jsshcmd/repl_help.go
@@ -5,13 +5,6 @@ import (
 )
 
 var replApiList = []string{
-	"__cpucount", "__os", "__arch", "__version", "__bin", "__pid", "__tmpdir", "__homedir", "__user", "__hostname",
-	"__dirname", "__filename", "__args", "__env", "__output", "__outputbytes", "__code", "global",
-	"__globalfiles",
-	"evalfile(", "format(", "print(", "println(", "stdoutlog(", "stderrlog(", "readline(", "sleep(", "exit(", "loadconfig(",
-	"base64encode(", "base64decode(", "md5(", "sha1(", "sha256(",
-	"randomstring(", "formatdate(",
-	"networkinterfaces(",
 	"fs.readdir(", "fs.readfile(", "fs.stat(", "fs.exist(", "fs.writefile(", "fs.appendfile(",
 	"path.join(", "path.abs(", "path.base(", "path.ext(", "path.dir(",
 	"cli.get(", "cli.bool(", "cli.args(", "cli.opts(", "cli.prompt(", "cli.subcommand(", "cli.subcommandstart(",
