82336f4e5c1fbb3356c6bbed5f2d2ba26ffc45f3
Zongmin Lei <leizongmin@gmail.com>
2020-12-15 17:44:23 +0800

    chore: fix build-release action

--- internal/jsshcmd/builtin_sh_module_pty_ignore.go
+++ internal/jsshcmd/builtin_sh_module_pty_ignore.go
@@ -1,4 +1,11 @@
+// +build windows
+
 package jsshcmd
+
+import (
+	"github.com/leizongmin/go/typeutil"
+	"github.com/leizongmin/jssh/internal/jsexecutor"
+)
 
 func jsFnShPty(global typeutil.H) jsexecutor.JSFunction {
 	return func(ctx *jsexecutor.JSContext, this jsexecutor.JSValue, args []jsexecutor.JSValue) jsexecutor.JSValue {
