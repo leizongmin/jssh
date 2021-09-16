cc53dbe01e97b54ba0deaa6f359925507d21cdfd
Zongmin Lei <leizongmin@gmail.com>
2021-09-17 00:49:57 +0800

    feat: repl 自动提示增加 http.get 等方法

--- internal/jsshcmd/repl_help.go
+++ internal/jsshcmd/repl_help.go
@@ -8,7 +8,7 @@ var replApiList = []string{
 	"fs.readdir(", "fs.readfile(", "fs.stat(", "fs.exist(", "fs.writefile(", "fs.appendfile(", "fs.readfilebytes(",
 	"path.join(", "path.abs(", "path.base(", "path.ext(", "path.dir(",
 	"cli.get(", "cli.bool(", "cli.args(", "cli.opts(", "cli.prompt(", "cli.subcommand(", "cli.subcommandstart(",
-	"http.timeout(", "http.request(", "http.download(",
+	"http.timeout(", "http.request(", "http.download(", "http.get(", "http.head(", "http.options(", "http.post(", "http.put(", "http.delete(",
 	"log.debug(", "log.info(", "log.warn(", "log.error(", "log.fatal(",
 	"setenv(", "exec(", "exec1(", "exec2", "bgexec(", "chdir(", "cd(", "cwd(", "pwd(",
 	"ssh.set(", "ssh.open(", "ssh.close(", "ssh.setenv(", "ssh.exec(", "ssh.exec1(", "ssh.exec2(",
