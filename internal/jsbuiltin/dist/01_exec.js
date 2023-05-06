a34206cdcd2eb12c951028de8b27475eeb2ef019
LEI Zongmin <leizongmin@gmail.com>
2023-05-06 23:24:07 +0800

    chore: add `jsbuiltin/dist`

---
+++ internal/jsbuiltin/dist/01_exec.js
@@ -0,0 +1,1 @@
+function exec1(cmd,env={}){return jssh.exec(cmd,env,1)}function exec2(cmd,env={}){return jssh.exec(cmd,env,2)}jssh.ssh.exec1=function(cmd,env={}){return jssh.ssh.exec(cmd,env,1)},jssh.ssh.exec2=function(cmd,env={}){return jssh.ssh.exec(cmd,env,2)},jssh.exec1=exec1,jssh.exec2=exec2,Object.freeze(jssh.ssh);
---
+++ internal/jsbuiltin/dist/99_bootstrap.js
@@ -0,0 +1,1 @@
+Object.freeze(jssh);for(const[name,value]of Object.entries(jssh))globalThis[name]=value;const global=globalThis||this;Object.defineProperty(global,"jssh",{value:jssh,writable:!1});
