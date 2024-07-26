1fa76ac18ffc22195fb476144127d290a6e34324
LEI Zongmin <leizongmin@gmail.com>
2024-07-26 23:11:29 +0800

    add qjs example

--- crates/jssh_core/src/runtime.rs
+++ crates/jssh_core/src/runtime.rs
@@ -58,8 +58,10 @@ impl RuntimeCore {
 
 #[cfg(test)]
 mod tests {
+    use std::ffi::{CStr, CString};
     use crate::runtime::*;
     use anyhow::Result;
+    use rquickjs::qjs;
 
     #[test]
     fn it_works() -> Result<()> {
@@ -82,6 +84,51 @@ mod tests {
                 eprintln!("eval error: {}", e)
             }
         };
+        Ok(())
+    }
+
+    #[test]
+    fn raw_qjs() -> Result<()> {
+        let rt = unsafe{ qjs::JS_NewRuntime() };
+        let ctx = unsafe { qjs::JS_NewContext(rt) };
+        let name = CString::new("anonymous")?;
+        let source:Vec<u8> = "throw new Error(123)".into();
+        let len = source.len();
+        let source = CString::new(source)?;
+        let flag = qjs::JS_EVAL_TYPE_MODULE | qjs::JS_EVAL_FLAG_STRICT;
+        let ret = unsafe { qjs::JS_Eval(ctx, source.as_ptr(), len as _, name.as_ptr(), flag as i32) };
+        let is_exception = unsafe {qjs::JS_IsException(ret)};
+        if is_exception {
+            let err_val = unsafe { qjs::JS_GetException(ctx) };
+            let err_raw = unsafe { qjs::JS_ToCString(ctx, err_val) };
+            let err = unsafe { CStr::from_ptr(err_raw) };
+            let err = err.to_string_lossy();
+            unsafe { qjs::JS_FreeCString(ctx, err_raw) };
+            unsafe { qjs::JS_FreeValue(ctx, err_val)};
+            eprintln!("eval error: {}", err);
+        } else {
+            let state = unsafe { qjs::JS_PromiseState(ctx, ret) };
+            match state {
+                qjs::JSPromiseStateEnum_JS_PROMISE_FULFILLED => { println!("promise fulfilled"); },
+                qjs::JSPromiseStateEnum_JS_PROMISE_REJECTED => {
+                    let reason_val = unsafe { qjs::JS_PromiseResult(ctx, ret) };
+                    let reason_raw = unsafe { qjs::JS_ToCString(ctx, reason_val) };
+                    let reason = unsafe { CStr::from_ptr(reason_raw) };
+                    let reason = reason.to_string_lossy();
+                    println!("promise rejected: {}", reason);
+                    unsafe { qjs::JS_FreeCString(ctx, reason_raw) };
+                    unsafe { qjs::JS_FreeValue(ctx, reason_val)};
+                },
+                qjs::JSPromiseStateEnum_JS_PROMISE_PENDING => { println!("promise pending"); },
+                _ => { println!("unknown promise state: {}", state); }
+            }
+
+            let result_raw = unsafe { qjs::JS_ToCString(ctx, ret) };
+            let result = unsafe { CStr::from_ptr(result_raw) };
+            let result = result.to_string_lossy();
+            println!("result: {:?}", result);
+            unsafe { qjs::JS_FreeCString(ctx, result_raw) };
+        }
         Ok(())
     }
 }
--- crates/qjs_example/src/main.rs
+++ crates/qjs_example/src/main.rs
@@ -1,6 +1,7 @@
 use rquickjs::{
-    embed, loader::Bundle, CatchResultExt, CaughtError, CaughtResult, Context, Module, Runtime,
+    embed, loader::Bundle, qjs, CatchResultExt, CaughtError, CaughtResult, Context, Module, Runtime,
 };
+use std::ffi::{CStr, CString};
 
 /// load the `my_module.js` file and name it myModule
 static BUNDLE: Bundle = embed! {
@@ -8,6 +9,55 @@ static BUNDLE: Bundle = embed! {
 };
 
 fn main() {
+    let rt = unsafe { qjs::JS_NewRuntime() };
+    let ctx = unsafe { qjs::JS_NewContext(rt) };
+    let name = CString::new("anonymous").unwrap();
+    let source: Vec<u8> = "throw new Error(123)".into();
+    let len = source.len();
+    let source = CString::new(source).unwrap();
+    let flag = qjs::JS_EVAL_TYPE_MODULE | qjs::JS_EVAL_FLAG_STRICT;
+    let ret = unsafe { qjs::JS_Eval(ctx, source.as_ptr(), len as _, name.as_ptr(), flag as i32) };
+    let is_exception = unsafe { qjs::JS_IsException(ret) };
+    if is_exception {
+        let err_val = unsafe { qjs::JS_GetException(ctx) };
+        let err_raw = unsafe { qjs::JS_ToCString(ctx, err_val) };
+        let err = unsafe { CStr::from_ptr(err_raw) };
+        let err = err.to_string_lossy();
+        unsafe { qjs::JS_FreeCString(ctx, err_raw) };
+        unsafe { qjs::JS_FreeValue(ctx, err_val) };
+        eprintln!("eval error: {}", err);
+    } else {
+        let state = unsafe { qjs::JS_PromiseState(ctx, ret) };
+        match state {
+            qjs::JSPromiseStateEnum_JS_PROMISE_FULFILLED => {
+                println!("promise fulfilled");
+            }
+            qjs::JSPromiseStateEnum_JS_PROMISE_REJECTED => {
+                let reason_val = unsafe { qjs::JS_PromiseResult(ctx, ret) };
+                let reason_raw = unsafe { qjs::JS_ToCString(ctx, reason_val) };
+                let reason = unsafe { CStr::from_ptr(reason_raw) };
+                let reason = reason.to_string_lossy();
+                println!("promise rejected: {}", reason);
+                unsafe { qjs::JS_FreeCString(ctx, reason_raw) };
+                unsafe { qjs::JS_FreeValue(ctx, reason_val) };
+            }
+            qjs::JSPromiseStateEnum_JS_PROMISE_PENDING => {
+                println!("promise pending");
+            }
+            _ => {
+                println!("unknown promise state: {}", state);
+            }
+        }
+
+        let result_raw = unsafe { qjs::JS_ToCString(ctx, ret) };
+        let result = unsafe { CStr::from_ptr(result_raw) };
+        let result = result.to_string_lossy();
+        println!("result: {:?}", result);
+        unsafe { qjs::JS_FreeCString(ctx, result_raw) };
+    }
+}
+
+fn main_1() {
     let rt = Runtime::new().unwrap();
     let ctx = Context::full(&rt).unwrap();
 
